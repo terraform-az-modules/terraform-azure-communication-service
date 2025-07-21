ffca866ce6fe95d04a5e1c873662e29420ce002a
Vedant <vedant.kalia@clouddrove.com>
2025-07-22 02:54:47 +0530

    feat : Updated Communication Service Module

--- examples/complete/example.tf
+++ examples/complete/example.tf
@@ -3,5 +3,26 @@ provider "azurerm" {
 }
 
 ##-----------------------------------------------------------------------------
-## Resources
+## Resource Group module call
+## Resource group in which all resources will be deployed.
 ##-----------------------------------------------------------------------------
+module "resource_group" {
+  source      = "terraform-az-modules/resource-group/azure"
+  version     = "1.0.0"
+  name        = "core"
+  environment = "dev"
+  location    = "eastus"
+  label_order = ["name", "environment", "location"]
+}
+
+##-----------------------------------------------------------------------------
+## Communication Service Module Call
+##-----------------------------------------------------------------------------
+module "azcommunication" {
+  source              = "./../../"
+  name                = "core"
+  environment         = "dev"
+  resource_group_name = module.resource_group.resource_group_name
+  location            = "India"
+  enable_domain       = true
+}
